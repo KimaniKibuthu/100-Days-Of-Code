becfd2f74387bf836a869203f37b0ed67c5b16e0
KimaniKibuthu <32309808+KimaniKibuthu@users.noreply.github.com>
2021-06-10 11:32:49 +0300

    improved model

--- Data Science Process quizzes/Disaster Pipeline/app/workspace/data/process_data.py
+++ Data Science Process quizzes/Disaster Pipeline/app/workspace/data/process_data.py
@@ -1,6 +1,9 @@
-import sys
 import sqlalchemy
 import pandas as pd
+
+messages_path = 'disaster_messages.csv'
+categories_path = 'disaster_categories.csv'
+database_path = 'sqlite:///df.db'
 
 
 def load_data(messages_filepath, categories_filepath):
@@ -8,24 +11,25 @@ def load_data(messages_filepath, categories_filepath):
     categories = pd.read_csv(categories_filepath)
     df = messages.merge(categories, on='id')
     return df
-    
+
 
 def clean_data(df):
     categories = df['categories'].str.split(';', expand=True)
     row = categories.iloc[0, :]
-    category_names = row.str.split('-', expand=True)[1].values
+    category_names = row.str.split('-', expand=True)[0].values
     categories.columns = category_names
-    for column in categories:
+
+    for column in categories.columns:
         # set each value to be the last character of the string
         categories[column] = categories[column].str.split('-', expand=True)[1]
-    
+
         # convert column from string to numeric
         categories[column] = categories[column].astype(str)
-    
+
     df.drop('categories', axis=1, inplace=True)
     df = pd.concat([df, categories], axis=1)
     df = df.drop_duplicates()
-    
+
     return df
 
 
@@ -35,29 +39,16 @@ def save_data(df, database_filename):
 
 
 def main():
-    if len(sys.argv) == 4:
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+    df = load_data(messages_path, categories_path)
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+    print('Cleaning data...')
+    df = clean_data(df)
 
-        print('Cleaning data...')
-        df = clean_data(df)
-        
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
-        print('Cleaned data saved to database!')
-    
-    else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+    print('Saving data...\n    DATABASE: {}'.format(database_path))
+    save_data(df, database_path)
+
+    print('Cleaned data saved to database!')
 
 
 if __name__ == '__main__':
--- Data Science Process quizzes/Disaster Pipeline/app/workspace/models/train_classifier.py
+++ Data Science Process quizzes/Disaster Pipeline/app/workspace/models/train_classifier.py
@@ -1,7 +1,6 @@
 import sys
 import joblib
 import pandas as pd
-import numpy as np
 import sqlalchemy
 
 import re
@@ -22,21 +21,28 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 
+database_path = 'sqlite:///df.db'
+model_path = 'model.pkl'
+
+
 
 def load_data(database_filepath):
     engine = sqlalchemy.create_engine(database_filepath).connect()
     df = pd.read_sql_table('df', engine)
-    x = df['message'].values
-    y = df[['related', 'request', 'offer',
-            'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
-            'security', 'military', 'child_alone', 'water', 'food', 'shelter',
-            'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid',
-            'infrastructure_related', 'transport', 'buildings', 'electricity',
-            'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
-            'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
-            'other_weather', 'direct_report']].values
 
-    return x, y
+    category_names = ['related', 'request', 'offer',
+                      'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+                      'security', 'military', 'child_alone', 'water', 'food', 'shelter',
+                      'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid',
+                      'infrastructure_related', 'transport', 'buildings', 'electricity',
+                      'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+                      'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+                      'other_weather', 'direct_report']
+
+    x = df['message'].values
+    y = df[category_names].values
+
+    return x, y, category_names
 
 
 def tokenize(text):
@@ -54,20 +60,14 @@ def tokenize(text):
 
 
 def build_model():
-    x, y = load_data(database_filepath)
-    x_train, x_test, y_train, y_test = train_test_split(x, y,
-                                                        train_size=0.8,
-                                                        random_state=42)
-
     pipeline = Pipeline([
-
-        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('count_vector', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
 
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
 
-    return pipeline.fit(x_train, y_train)
+    return pipeline
 
 
 def evaluate_model(model, x_test, y_test, category_names):
@@ -84,31 +84,23 @@ def save_model(model, model_filepath):
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        x, y, category_names = load_data(database_filepath)
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    print('Loading data...\n    DATABASE: {}'.format(database_path))
+    x, y, category_names = load_data(database_path)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-        print('Building model...')
-        model = build_model()
+    print('Building model...')
+    model = build_model()
 
-        print('Training model...')
-        model.fit(x_train, y_train)
+    print('Training model...')
+    model.fit(x_train, y_train)
 
-        print('Evaluating model...')
-        evaluate_model(model, x_test, y_test, category_names)
+    print('Evaluating model...')
+    evaluate_model(model, x_test, y_test, category_names)
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+    print('Saving model...\n    MODEL: {}'.format(model_path))
+    save_model(model, model_path)
 
-        print('Trained model saved!')
-
-    else:
-        print('Please provide the filepath of the disaster messages database ' \
-              'as the first argument and the filepath of the pickle file to ' \
-              'save the model to as the second argument. \n\nExample: python ' \
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+    print('Trained model saved!')
 
 
 if __name__ == '__main__':
